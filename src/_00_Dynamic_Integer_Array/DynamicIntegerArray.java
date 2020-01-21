9611cf890c335b820f92366bb0a7bc8887369b17
League Student <league@WTS14-3.attlocal.net>
2020-01-20 18:23:28 -0800

    DIA(IP)

--- src/_00_Dynamic_Integer_Array/DynamicIntegerArray.java
+++ src/_00_Dynamic_Integer_Array/DynamicIntegerArray.java
@@ -7,7 +7,7 @@ public class DynamicIntegerArray {
 	public DynamicIntegerArray() {
 		//2. Initialize the int array to have 0 elements. 
 		//   This will prevent a null pointer exception
-	
+	int[]
 	}
 	
 	//3. Complete the steps in the add method
