6e5236007dd35aa90f915c3b86f39f56a1fd52e9
League Student <league@iMac-21.attlocal.net>
2020-01-22 19:40:58 -0800

    DIA(IP)

--- src/_00_Dynamic_Integer_Array/DynamicIntegerArray.java
+++ src/_00_Dynamic_Integer_Array/DynamicIntegerArray.java
@@ -2,26 +2,30 @@ package _00_Dynamic_Integer_Array;
 
 public class DynamicIntegerArray {
 	//1. Create a private int array. Don't initialize it.
-	int[] dexter 
+	int[] x;
 	
 	public DynamicIntegerArray() {
 		//2. Initialize the int array to have 0 elements. 
 		//   This will prevent a null pointer exception
-	int[]
+	int[]x = new int [0];
 	}
 	
 	//3. Complete the steps in the add method
 	public void add(int v) {
 		//A. create and initialize a new int array to be one 
 		//   element longer than the member array
-		
+		int[] y = new int[v + 1]; 
 		//B. set the last element of your new array to 
 		//   the value passed into the method
+	for (int i = 0; i < y.length; i++) {
 		
+	}
 		//C. iterate through the member array and 
 		//   copy every element from the member array 
 		//   to the new array
-		
+		for (int i = 0; i < y.length; i++) {
+			
+		}
 		//D. set the member array equal to the new array.
 	}
 	
